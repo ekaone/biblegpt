4751543947017a9666ed469b9cd25ea89dc8e860
Eka Prasetia <ekaone3033@gmail.com>
2023-03-29 09:57:41 +0700

    update token

--- pages/index.tsx
+++ pages/index.tsx
@@ -78,7 +78,7 @@ const Home: NextPage = () => {
       </Head>
 
       <Header />
-      <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-2 sm:mt-20">
+      <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-12 sm:mt-20">
         <div className="max-w-xl w-full">
           <div className="flex mt-10 items-center space-x-3">
             <Image
